afc56ced5f61accbfc412a1d8a4fe516a238928d
Tomas Hernandez <tomasgabrielh@gmail.com>
2026-05-25 18:37:42 -0300

    Se completaron modulos faltantes del sistema al igual que se implemento la modularizacion, se crearon funciones y se optimizaron funcionalidades del codigo

--- password_manager.py
+++ password_manager.py
@@ -1,23 +1,108 @@
 manager = []
 
-while True:
+# CREAR NUEVA CONTRASEÑA
 
-    opciones = 0
-
-    if opciones < 6:
-    
-        print()
-        print("===========================================")
-        print("Seleccione alguna de las opciones del menú")
-        print("===========================================")
-        print()
-        print("1. Nueva contraseña.")
-        print("2. Editar contraseña.")
-        print("3. Eliminar contraseña.")
-        print("4. Ver contraseña.")
-        print("5. Buscar contraseña.")
+def nueva_contrasena(user, account, password):
         
-        menu = input("Elija una opción")
+    if account == "" or user == "" or password == "":
+        print("No ingresaste ningun dato...")
+        return
+    
+    nueva_credencial = {
+        "usuario" : user,
+        "cuenta" : account,
+        "contrasena" : password
+    }
+    
+    manager.append(nueva_credencial)
+    print("¡Credencial guardada con éxito!")
+    
+# EDITAR CONTRASEÑA
+        
+def editar_contrasena(lista_origen, cuenta_a_editar):
+    
+    encontrado = False
+
+    for credencial in lista_origen:
+        if credencial["cuenta"] == cuenta_a_editar:
+            print("Registro encontrado. Ingrese los nuevos datos.")
+            
+            nuevo_account = input("Ingrese el nuevo nombre de la cuenta: ").strip().title()
+            nuevo_user = input("Ingrese los nuevos datos de usuario: ").strip().title()
+            nuevo_pass = input("Ingrese la nueva contraseña: ").strip()
+            
+            credencial["usuario"] = nuevo_user
+            credencial["contrasena"] = nuevo_pass
+            credencial["cuenta"] = nuevo_account
+            
+            encontrado = True
+            break
+        
+    if encontrado == False:
+        print("La cuenta ingresada no existe")
+    
+# ELIMINAR CONTRASEÑA
+    
+def eliminar_contrasena(lista_origen, cuenta_a_eliminar):
+    
+    encontrado = False
+
+    for credencial in lista_origen:
+        if credencial["cuenta"] == cuenta_a_eliminar:
+            lista_origen.remove(credencial)
+            print("¡Credencial eliminada con éxito!")
+            encontrado = True
+            break
+    
+    if encontrado == False:
+        print("No se encontro ningun registro")
+        
+    
+# VER CONTRASEÑA
+    
+def ver_contrasena(lista_origen):
+    
+    if lista_origen:
+        print("Se encontraron registros")
+        for credencial in lista_origen:
+            print(f"Cuenta: {credencial['cuenta']} | Usuario: {credencial['usuario']} | Contraseña: {credencial['contrasena']}")
+    else:
+        print("No hay contraseñas guardadas en el sistema todavía.")
+    
+def buscar_contrasena(lista_origen, cuenta_buscada):
+    
+    encontrado = False
+
+    for credencial in lista_origen:
+        
+        if credencial["cuenta"] == cuenta_buscada:
+            print("\n=== CREDENCIAL ENCONTRADA ===")
+            encontrado = True
+            print(f"Cuenta: {credencial['cuenta']} | Usuario: {credencial['usuario']} | Contraseña: {credencial['contrasena']}")
+        
+    if encontrado == False:
+        print("No se encontraron registros asociados a tu busqueda")
+        
+    
+print("=====================================")
+print("Sistema para gestion de contraseñas")
+print("=====================================")
+
+while True:
+    
+    print()
+    print("===========================================")
+    print("Seleccione alguna de las opciones del menú")
+    print("===========================================")
+    print()
+    print("1. Nueva contraseña.")
+    print("2. Editar contraseña.")
+    print("3. Eliminar contraseña.")
+    print("4. Ver contraseña.")
+    print("5. Buscar contraseña.")
+    print("6. Salir del sistema.")
+    
+    menu = input("Elija una opción")
         
     match menu:
         case "1":
@@ -29,7 +114,7 @@ while True:
             pregunta = input("¿Desea crear una nueva contraseña? (Escriba 'Si' para continuar o 'No' para salir)").strip().title()
             
             if pregunta == "Si":
-                
+        
                 print("Paso 1: Ingrese el nombre de la cuenta")
                 account = input("Ingrese el nombre de la cuenta: ").strip().title()
                 
@@ -37,19 +122,11 @@ while True:
                 user = input("Ingrese mail o nombre de usuario: ").strip().title()
                 
                 print("Paso 1: Ingrese la contreaseña del usuario")
-                password = input("Ingrese su contraseña (Debe contener solo números): ")
-                
-                if account == "" or user == "" or password == "":
-                    print("No ingresaste ningun dato...")
-                    continue
-                
-                manager.append(user)
-                manager.append(account)
-                manager.append(password)
-                print("¡Credencial guardada con éxito!")
+                password = input("Ingrese su contraseña: ").strip()
             
+                nueva_contrasena(user, account, password)
             else:
-                print("Operación cancelada.")
+                print("Operacion cancelada")
                 
         case "2":
             
@@ -57,11 +134,17 @@ while True:
             print("Editar contraseña.")
             print("======================")
             
+            cuenta_modificar = input("¿Qué datos deseas modificar?: ").strip().title()
+            editar_contrasena(manager, cuenta_modificar)
+            
         case "3":
             
             print("======================")
             print("Eliminar contraseña.")
             print("======================")
+            
+            cuenta_borrar = input("¿Qué cuenta desea eliminar?: ").strip().title()
+            eliminar_contrasena(manager, cuenta_borrar)
             
         case "4":
             
@@ -69,17 +152,22 @@ while True:
             print("Ver contraseña.")
             print("======================")
             
+            ver_contrasena(manager)
+            
         case "5":
             
             print("======================")
             print("Buscar contraseña.")
             print("======================")
             
+            pregunta = input("¿Que cuenta esta buscando?").strip().title()
+            
+            buscar_contrasena(manager, pregunta)
+            
+        case "6":
+        
+            print("Saliendo del sistema...")
+            break
+            
         case  _:
             print("Opción incorrecta")
-            
-            
-            
-print("=====================================")
-print("Sistema para gestion de contraseñas")
-print("=====================================")
